77ec2d1cf2990849fd4c67f198afe32ea66fc707
woong99 <dndry1072@naver.com>
2022-03-04 17:08:44 +0900

    Todo

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
+import SignUp from './pages/SignUp';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,50 +1,32 @@
-import { useState, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useCallback } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { loginEmail, signupEmail } from '../firebase';
 import '../styles/Login.scss';
-
-const Login = () => {
+const SignUp = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
-  const [password, setPassWord] = useState('');
+  const [password, setPassword] = useState('');
 
   const onChange = useCallback((e) => {
     e.target.type === 'email'
       ? setEmail(e.target.value)
-      : setPassWord(e.target.value);
+      : setPassword(e.target.value);
   }, []);
 
-  // 회원가입
   const onSignUp = (e) => {
-    navigate('./signup');
-    // e.preventDefault();
-    // console.log(email);
-    // console.log(password);
-    // signupEmail(email, password)
-    //   .then((result) => {
-    //     const user = result.user;
-    //   })
-    //   .catch((error) => console.log(error));
-  };
-
-  // 로그인
-  const onSignIn = (e) => {
     e.preventDefault();
-    loginEmail(email, password)
+    signupEmail(email, password)
       .then((result) => {
-        const uid = result.user.uid;
-        navigate('./main', { state: { uid } });
+        const user = result.user;
+        alert('회원가입되었습니다!');
+        navigate('/');
       })
-      .catch((error) => {
-        console.log(error);
-        alert('아이디 혹은 비밀번호가 잘못되었습니다.');
-      });
+      .catch((error) => console.log(error));
   };
-
   return (
     <Form className="form">
-      <h1>To Do!!</h1>
+      <h1>회원가입</h1>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>E-mail</Form.Label>
         <Form.Control
@@ -62,13 +44,8 @@ const Login = () => {
           onChange={onChange}
         />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+
       <div className="buttons">
-        <Button variant="primary" type="submit" onClick={onSignIn}>
-          Sign In
-        </Button>
         <Button variant="primary" type="submit" onClick={onSignUp}>
           Sign Up
         </Button>
@@ -77,4 +54,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
